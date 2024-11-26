cf60d5b025e02a3b6e534179a8a9c3913753a223
JoaoVictor-dev-jv <victor.sena12@hotmail.com>
2024-11-26 12:37:58 -0300

    Alteração do código

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+const buttonConsult = document.querySelector(".btn")
+const result = document.querySelector(".result")
+
 function generateNumber() {
-    const min = Math.ceil(document.querySelector(".input-min").value)
-    const max = Math.floor(document.querySelector(".input-max").value)
+    const min = Math.ceil(document.querySelector(".input-min").value);
+    const max = Math.floor(document.querySelector(".input-max").value);
 
-    const result = Math.floor(Math.random() * (max - min) + min);
+    if (min >= max) {
+        alert("Valor mínimo deve ser menor que o valor máximo");
+    }
 
-    alert(result)
+    else {
+        const result = Math.floor(Math.random() * (max - min) + min);
+        document.querySelector(".result").value = result;
+    }
+    // alert(result)
 }
+
+buttonConsult.addEventListener("click", generateNumber);
